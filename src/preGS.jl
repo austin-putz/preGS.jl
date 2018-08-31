a40165b9cfeb19a867119f856fa7d913bd905f65
Austin Putz <putz.austin@gmail.com>
2018-08-31 16:30:31 -0500

    created first preGS.jl file

---
+++ src/preGS.jl
@@ -0,0 +1,17 @@
+__precompile__(true)
+
+module preGS
+
+using DataFrames
+using UnicodePlots
+using ProgressMeter
+using CSV
+using Printf
+using Distributed
+using Statistics
+using DelimitedFiles
+using LinearAlgebra
+
+include("preGS_current.jl")
+
+end
